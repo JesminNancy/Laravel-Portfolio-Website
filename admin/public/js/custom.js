1e3d6d52bbe53288ff97aff9fd94f4b9ea08ab0c
JesminNancy <jesminnancy@hotmail.com>
2022-06-21 23:10:22 +0600

    Courses Section- Admin panel(Add/Delete)

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -26,7 +26,11 @@ function getCoursesData() {
               });
               
             //Service Table Delete Icon Click
-
+                $('.courseDeleteBtn').click(function(){
+                    var id = $(this).data('id');
+                    $('#courseDeleteBtn').html(id);
+                    $('#deleteCourseModal').modal('show');
+                })
               
               //Service Table Edit Icon Click
 
@@ -43,3 +47,123 @@ function getCoursesData() {
       });
   
   }
+  
+  // Courses Add New btn Click
+  $('#addNewCourseBtnId').click(function(){
+    $('#addCourseModal').modal('show');
+  });
+  
+  // Courses Add Modal Save Btn
+$('#CourseAddConfirmBtn').click(function() {
+    var CourseName = $('#CourseNameId').val();
+    var CourseDes = $('#CourseDesId').val();
+    var CourseFee = $('#CourseFeeId').val();
+    var CourseEnroll = $('#CourseEnrollId').val();
+    var CourseClass = $('#CourseClassId').val();
+    var CourseLink = $('#CourseLinkId').val();
+    var CourseImg = $('#CourseImgId').val();
+    CoursesAdd(CourseName,CourseDes,CourseFee,CourseEnroll,CourseClass,CourseLink,CourseImg);
+})
+
+// Service Add Method
+
+function CoursesAdd(CourseName,CourseDes,CourseFee,CourseEnroll,CourseClass,CourseLink,CourseImg) {
+
+  
+    if(CourseName.length==0){
+        // toastr.error('serviceName is Empty');
+        alert('CourseName is Empty');
+    }
+    else if(CourseDes.length==0){
+        // toastr.error('serviceDes is Empty');
+        alert('CourseDes is Empty');
+    }
+    else if(CourseFee.length==0){
+     // toastr.error('serviceImg is Empty');
+     alert('CourseFee is Empty');
+     }
+    else if(CourseEnroll.length==0){
+        // toastr.error('serviceImg is Empty');
+         alert('CourseEnroll is Empty');
+    }
+     else if(CourseClass.length==0){
+        // toastr.error('serviceImg is Empty');
+        alert('CourseClass is Empty');
+     }
+     else if(CourseLink.length==0){
+        // toastr.error('serviceImg is Empty');
+        alert('CourseLink is Empty');
+     }
+     else if(CourseImg.length==0){
+        // toastr.error('serviceImg is Empty');
+         alert('CourseImg is Empty');
+    }else{
+        $('#CourseAddConfirmBtn').html("<div class='spinner-border spinner-border-sm' role='status'></div>");//Animation
+        axios.post('/CoursesAdd', {
+            course_name:CourseName,
+            course_des:CourseDes,
+            course_fee:CourseFee,
+            course_totalenroll:CourseEnroll,
+            course_totalclass:CourseClass,
+            course_link:CourseLink,
+            course_img:CourseImg,
+        })
+        .then(function(response) {
+            if(response.status==200){
+                $('#CourseAddConfirmBtn').html("Save");
+                if (response.data == 1) {
+                    $('#addCourseModal').modal('hide');
+                    // toastr.success('Update Success');
+                    alert('Add Success');
+                    getCoursesData();
+                } else {
+                    $('#addCourseModal').modal('hide');
+                    // toastr.error('Update Fail');
+                    alert('Add Fail!')
+                    getCoursesData();
+                }
+            }
+    
+        })
+        .catch(function(error) {
+            $('#addCourseModal').modal('hide');
+            alert('Something Went Wrong!')
+        });
+    
+    }
+ }
+    
+//Courses Delete Modal Yes Button
+$('#courseDeleteConfirmBtn').click(function() {
+    var id = $('#courseDeleteBtn').html();
+    CourseDelete(id);
+});
+
+//Course Delete
+function CourseDelete(deleteid) {
+    $('#courseDeleteConfirmBtn').html("<div class='spinner-border spinner-border-sm' role='status'></div>");//Animation
+  axios.post('/CoursesDelete', {
+          id: deleteid
+      })
+      .then(function(response) {
+          if (response.data == 1) {
+            $('#courseDeleteConfirmBtn').html("Yes");
+              $('#deleteCourseModal').modal('hide');
+              // toastr.success('Delete Success');
+              alert('Delete Success');
+              getCoursesData();
+          } else {
+              $('#deleteCourseModal').modal('hide');
+              // toastr.success('Delete Fail');
+              alert('Delete Fail');
+              getCoursesData();
+          }
+  
+      })
+      .catch(function(error) {
+        $('#deleteCourseModal').modal('hide');
+        // toastr.success('Delete Fail');
+        alert('Something Went Wrong!');
+      });
+  
+  }    
--- admin/resources/views/Courses.blade.php
+++ admin/resources/views/Courses.blade.php
@@ -7,6 +7,9 @@
 <div id="mainDivCourse" class="container  d-none">
   <div class="row">
   <div class="col-md-12 p-5">
+  
+    <button id="addNewCourseBtnId" class="btn my-3 btn-sm btn-danger">Add New </button>
+    
   <table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
     <thead>
       <tr>
@@ -55,11 +58,58 @@
     </div>
   </div>
 
-
-
 @endsection
 
 
+<div class="modal fade" id="addCourseModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title">Add New Course</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body  text-center">
+       <div class="container">
+       	<div class="row">
+       		<div class="col-md-6">
+             	<input id="CourseNameId" type="text" id="" class="form-control mb-3" placeholder="Course Name">
+          	 	<input id="CourseDesId" type="text" id="" class="form-control mb-3" placeholder="Course Description">
+    		 	<input id="CourseFeeId" type="text" id="" class="form-control mb-3" placeholder="Course Fee">
+     			<input id="CourseEnrollId" type="text" id="" class="form-control mb-3" placeholder="Total Enroll">
+       		</div>
+       		<div class="col-md-6">
+     			<input id="CourseClassId" type="text" id="" class="form-control mb-3" placeholder="Total Class">      
+     			<input id="CourseLinkId" type="text" id="" class="form-control mb-3" placeholder="Course Link">
+     			<input id="CourseImgId" type="text" id="" class="form-control mb-3" placeholder="Course Image">
+       		</div>
+       	</div>
+       </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Cancel</button>
+        <button  id="CourseAddConfirmBtn" type="button" class="btn  btn-sm  btn-danger">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="deleteCourseModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body text-center p-3">
+        <h5 class="mt-4">Are You Sure to Delete?</h5>
+        <h6 id="courseDeleteBtn"></h6>
+      </div>
+      <div class="modal-footer">
+        <button type="button"  id="courseDeleteConfirmBtn" class="btn btn-primary btn-sm">Yes</button>
+        <button type="button" class="btn btn-danger btn-sm" data-mdb-dismiss="modal">No</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 @section('script')
